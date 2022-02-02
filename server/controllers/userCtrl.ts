e92cd260a24947bbbd4e11a74cfbf337e317e488
Giovanni Ferioli <gferioli0418@gmail.com>
2022-02-02 16:11:12 -0500

    Update user and added more routes

--- server/controllers/userCtrl.ts
+++ server/controllers/userCtrl.ts
@@ -1,55 +1,109 @@
-import { Request, Response } from 'express'
-import { IReqAuth } from '../config/interface'
-import Users from '../models/userModel'
-import bcrypt from 'bcrypt'
-
+import { Request, Response } from "express";
+import { IReqAuth } from "../config/interface";
+import Users from "../models/userModel";
+import bcrypt from "bcrypt";
 
 const userCtrl = {
-  updateUser: async (req: IReqAuth, res: Response) => {
-    if(!req.user) return res.status(400).json({msg: "Invalid Authentication."})
-    
-    try {
-      const { name } = req.body
+	updateUser: async (req: IReqAuth, res: Response) => {
+		if (!req.user)
+			return res.status(400).json({ msg: "Invalid Authentication." });
 
-      await Users.findOneAndUpdate({_id: req.user._id}, {
-         name
-      })
+		try {
+			const { name } = req.body;
 
-      res.json({ msg: "Update Success!" })
-    } catch (err: any) {
-      return res.status(500).json({msg: err.message})
-    }
-  },
-  resetPassword: async (req: IReqAuth, res: Response) => {
-    if(!req.user) return res.status(400).json({msg: "Invalid Authentication."})
-    
-    if(req.user.type !== 'register')
-      return res.status(400).json({
-        msg: `Quick login account with ${req.user.type} can't use this function.`
-      })
+			await Users.findOneAndUpdate(
+				{ _id: req.user._id },
+				{
+					name,
+				}
+			);
 
-    try {
-      const { password } = req.body
-      const passwordHash = await bcrypt.hash(password, 12)
+			res.json({ msg: "Update Success!" });
+		} catch (err: any) {
+			return res.status(500).json({ msg: err.message });
+		}
+	},
 
-      await Users.findOneAndUpdate({_id: req.user._id}, {
-        password: passwordHash
-      })
+	resetPassword: async (req: IReqAuth, res: Response) => {
+		if (!req.user)
+			return res.status(400).json({ msg: "Invalid Authentication." });
 
-      res.json({ msg: "Reset Password Success!" })
-    } catch (err: any) {
-      return res.status(500).json({msg: err.message})
-    }
-  },
-  getUser: async (req: Request, res: Response) => {
-    try {
-      const user = await Users.findById(req.params.id).select('-password')
-      res.json(user)
-    } catch (err: any) {
-      return res.status(500).json({msg: err.message})
-    }
-  }
-}
+		if (req.user.type !== "register")
+			return res.status(400).json({
+				msg: `Quick login account with ${req.user.type} can't use this function.`,
+			});
 
+		try {
+			const { password } = req.body;
+			const passwordHash = await bcrypt.hash(password, 12);
+
+			await Users.findOneAndUpdate(
+				{ _id: req.user._id },
+				{
+					password: passwordHash,
+				}
+			);
+
+			res.json({ msg: "Reset Password Success!" });
+		} catch (err: any) {
+			return res.status(500).json({ msg: err.message });
+		}
+	},
+
+	getUser: async (req: Request, res: Response) => {
+		try {
+			const user = await Users.findOne({ account: req.body.account }).select(
+				"-password"
+			);
+			if (!user) return res.status(404).json();
+
+			res.json(user);
+		} catch (err: any) {
+			return res.status(500).json({ msg: err.message });
+		}
+	},
+
+	createUser: async (req: Request, res: Response) => {
+		const userFound = await Users.findOne({
+			account: req.body.account,
+		});
+
+		if (userFound)
+			return res.status(303).json({ message: "The User already exists" });
+
+		const passwordHash = await bcrypt.hash(req.body.password, 12);
+		const newUser = new Users({
+			name: req.body.name,
+			account: req.body.account,
+			password: passwordHash,
+			role: "",
+			type: "",
+			location: req.body.Location,
+			intro: "",
+			Friends: [],
+			Groups: [],
+			// TODO: Add other profile properties
+		});
+		try {
+			const user = await newUser.save();
+			res.json(user);
+		} catch (err: any) {
+			return res.status(500).json({ msg: err.message });
+		}
+	},
+
+	deleteUser: async (req: Request, res: Response) => {
+		try {
+			const profileFound = await Users.findOneAndRemove({
+				account: req.body.account,
+			});
+			if (!profileFound) return res.status(404).json();
+
+			return res.status(204).json();
+		} catch (err: any) {
+			return res.status(500).json({ msg: err.message });
+		}
+	},
+};
 
 export default userCtrl;
--- server/index.ts
+++ server/index.ts
@@ -1,39 +1,35 @@
-
-import "dotenv/config"
+import "dotenv/config";
 require("dotenv");
 
-import express from 'express'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
-import morgan from 'morgan'
-import routes from './routes/index'
-import { createServer } from 'http'
-
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import routes from "./routes/index";
+import { createServer } from "http";
 
 // Middleware
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cors())
-app.use(morgan('dev'))
-app.use(cookieParser())
-
-
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(morgan("dev"));
+app.use(cookieParser());
 
 // Socket.io
-const http = createServer(app)
+const http = createServer(app);
 
 // Routes
-app.use('/', routes.authRouter)
-app.use('/', routes.userRouter)
-app.use('/', routes.articleRouter)
+app.use("/api/auth", routes.authRouter);
+app.use("/api/user", routes.userRouter);
+app.use("/api/article", routes.articleRouter);
 
 // Database
-import './config/database'
-import { Mongoose } from 'mongoose';
+import "./config/database";
+import { Mongoose } from "mongoose";
 
 // server listenning
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 http.listen(PORT, () => {
-  console.log('Server is running on port', PORT)
-})
+	console.log("Server is running on port", PORT);
+});
